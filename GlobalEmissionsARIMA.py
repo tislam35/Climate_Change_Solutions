3830615b81dc7850ff18001092bd70d0ffd4b265
Tohidul Islam <tislam35@gmail.com>
2021-10-23 12:09:59 -0400

    added basic graph

--- GlobalEmissionsARIMA.py
+++ GlobalEmissionsARIMA.py
@@ -8,4 +8,18 @@ import warnings
 import itertools
 warnings.filterwarnings("ignore")
 
+#Loading in the data
+dateparse = lambda x: pd.to_datetime(x, format='%Y', errors = 'coerce')
+data = pd.read_csv("Datasets/GlobalEmissions.csv", parse_dates=['Year'], index_col='Year', date_parser=dateparse)
+print(data.index, data.values)
+data.info()
+
+#Testing data with basic graph
+fig, ax = plt.subplots()
+ax.plot(data.index, data.values)
+ax.set_xlabel("Time (Yearly)")
+ax.set_ylabel("Million tonnes of carbon dioxide")
+ax.set_title("Global carbon emissions since 1965")
+plt.show()
+
 #
