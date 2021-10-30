8fe9e019e26572669bca3a86e93b41acc2386711
Tohidul Islam <tislam35@gmail.com>
2021-10-30 13:38:38 -0400

    Created function to get optimal PDQ and related Model

--- GlobalEmissionsARIMA.py
+++ GlobalEmissionsARIMA.py
@@ -17,48 +17,80 @@ print(data.index, data.values)
 data.info()
 
 #Testing data with basic graph
-fig1, ax1 = plt.subplots()
-ax1.plot(data)
-ax1.set_xlabel("Time (Yearly)")
-ax1.set_ylabel("Million tonnes of carbon dioxide")
-ax1.set_title("Global carbon emissions since 1965")
+fig, ax = plt.subplots()
+ax.plot(data)
+ax.set_xlabel("Time (Yearly)")
+ax.set_ylabel("Million tonnes of carbon dioxide")
+ax.set_title("Global carbon emissions since 1965")
 plt.show()
 
 #Making data stationary
 movAve = data.rolling(5).mean()
-fig2, ax2 = plt.subplots()
-ax2.plot(data)
-ax2.plot(movAve, color="red")
-ax2.set_xlabel("Time (Yearly)")
-ax2.set_ylabel("Million tonnes of carbon dioxide")
-ax2.set_title("Global carbon emissions since 1965")
+fig, ax = plt.subplots()
+ax.plot(data)
+ax.plot(movAve, color="red")
+ax.set_xlabel("Time (Yearly)")
+ax.set_ylabel("Million tonnes of carbon dioxide")
+ax.set_title("Global carbon emissions since 1965")
 plt.show()
 stationary_data = data - movAve
-fig3, ax3 = plt.subplots()
-ax3.plot(data)
-ax3.plot(movAve, color="red")
-ax3.plot(stationary_data, color="green")
-ax3.set_xlabel("Time (Yearly)")
-ax3.set_ylabel("Million tonnes of carbon dioxide")
-ax3.set_title("Global carbon emissions since 1965")
-plt.show()
-
-#Getting ARIMA model at an arbitrary p,d,q value and forcasting next 10 years
-predictions = list()
-values = data.values
-for i in range(0,10):
-    model = ARIMA(values, order=(5,1,5))
-    accuracy = model.fit()
-    predictions.append(accuracy.forecast()[0])
-    values= np.append(values, (accuracy.forecast()[0]))
-    print(predictions)
-fig4, ax4 = plt.subplots()
-ax4.plot(data)
-dates = pd.DatetimeIndex(['2020-01-01', '2021-01-01', '2022-01-01', '2023-01-01', '2024-01-01', '2025-01-01', '2026-01-01', '2027-01-01', '2028-01-01', '2029-01-01'])
-ax4.plot(dates, predictions, color="red")
-ax4.set_xlabel("Time (Yearly)")
-ax4.set_ylabel("Million tonnes of carbon dioxide")
-ax4.set_title("Global carbon emissions since 1965")
+fig, ax = plt.subplots()
+ax.plot(data)
+ax.plot(movAve, color="red")
+ax.plot(stationary_data, color="green")
+ax.set_xlabel("Time (Yearly)")
+ax.set_ylabel("Million tonnes of carbon dioxide")
+ax.set_title("Global carbon emissions since 1965")
 plt.show()
 
 #Finding optimal p, d, q values
+def optimParam(curData):
+    lowest_aic = float("inf")
+    optimal_params = list()
+    results = None
+    p = d = q = range(0, 2)
+    pdq = list(itertools.product(p,d,q))
+    for i in pdq:
+        model = ARIMA(curData, order=i, enforce_stationarity=False, enforce_invertibility=False)
+        accuracy = model.fit().aic
+        if(accuracy < lowest_aic):
+            lowest_aic = accuracy
+            optimal_params = i
+            results = model.fit()
+    results.plot_diagnostics(figsize=(15,12))
+    plt.show()
+    return optimal_params
+
+#Getting ARIMA model forecast for given number of years
+#def getModel(years):
+#    predictions = list()
+#    values = data.values
+#    for i in range(0,years):
+#        model = ARIMA(values, order=optimParam(values), enforce_stationarity=False, enforce_invertibility=False)
+#        nextGuess = model.fit().forecast()[0]
+#        predictions.append(nextGuess)
+#        values= np.append(values, nextGuess)
+#    fig, ax = plt.subplots()
+#    ax.plot(data)
+#    dates = pd.date_range(start="01/01/2020", periods=years, freq='YS')
+#    ax.plot(dates, predictions, color="red")
+#    ax.set_xlabel("Time (Yearly)")
+#    ax.set_ylabel("Million tonnes of carbon dioxide")
+#    ax.set_title("Global carbon emissions since 1965")
+#    plt.show()
+#getModel(10)
+
+def getModel(targetYear):
+    results = ARIMA(data, order=optimParam(data), enforce_stationarity=False, enforce_invertibility=False).fit()
+    predictions = results.get_prediction(start=data.index[0], end=str(targetYear), dynamic=False)
+    fig, ax = plt.subplots()
+    ax.plot(data, label="Observed")
+    ax.plot(predictions.predicted_mean, label="Predicted")
+    ax.fill_between(predictions.conf_int().index, predictions.conf_int().iloc[:, 0], predictions.conf_int().iloc[:, 1], color='green', alpha=.5)
+    ax.set_xlabel("Time (Yearly)")
+    ax.set_ylabel("Million tonnes of carbon dioxide")
+    ax.set_title("Global carbon emissions since 1965")
+    plt.legend()
+    plt.show()
+
+getModel(2050)
